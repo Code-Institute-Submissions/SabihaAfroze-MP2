14a1a6b01b9ad78fecc175afc9af001cf7af3006
Sabiha <fosabiha@gmail.com>
2020-10-15 02:09:21 +0000

    quiztoddler.js is coded partially

--- assets/js/quiztoddler.js
+++ assets/js/quiztoddler.js
@@ -1,10 +1,52 @@
-var myQuestions = [
-	{
-		question: "What is 10/2?",
-		answers: {
-			a: '3',
-			b: '5',
-			c: '115'
-		},
-		correctAnswer: 'b'
-	}
+function check(){
+
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+	var question5 = document.quiz.question5.value;
+    var question6 = document.quiz.question6.value;
+    var question7 = document.quiz.question7.value;
+	var question8 = document.quiz.question8.value;
+    var question9 = document.quiz.question9.value;
+    var question10 = document.quiz.question10.value;
+    
+    var correct = 0;
+    
+    if (question1 == "2") {
+		correct++;
+    }
+	if (question2 == "2") {
+		correct++;
+    }	
+	if (question3 == "1") {
+		correct++;
+    }
+
+    if (question4 == "1") {
+		correct++;
+    }
+    
+    if (question5 == "2") {
+		correct++;
+    }
+
+    if (question6 == "3") {
+		correct++;
+    }
+
+    if (question7 == "2") {
+		correct++;
+    }
+
+    if (question8 == "2") {
+		correct++;
+    }
+
+    if (question9 == "2") {
+		correct++;
+    }
+
+    if (question10 == "2") {
+		correct++;
+    }
