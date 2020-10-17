0f6905bbf613e14fa23d1867f5b997074d1eb47b
Sabiha <fosabiha@gmail.com>
2020-10-17 01:13:36 +0000

    quiztoddler.js is updated and all console eroor solved and not its working!

--- assets/js/quiztoddler.js
+++ assets/js/quiztoddler.js
@@ -1,15 +1,15 @@
 function check(){
 
-	var question1 = document.math-quiz.question1.value;
-	var question2 = document.math-quiz.question2.value;
-    var question3 = document.math-quiz.question3.value;
-    var question4 = document.math-quiz.question4.value;
-	var question5 = document.math-quiz.question5.value;
-    var question6 = document.math-quiz.question6.value;
-    var question7 = document.math-quizz.question7.value;
-	var question8 = document.math-quiz.question8.value;
-    var question9 = document.math-quiz.question9.value;
-    var question10 = document.math-quiz.question10.value;
+	var question1 = document.mathquiz.question1.value;
+	var question2 = document.mathquiz.question2.value;
+    var question3 = document.mathquiz.question3.value;
+    var question4 = document.mathquiz.question4.value;
+	var question5 = document.mathquiz.question5.value;
+    var question6 = document.mathquiz.question6.value;
+    var question7 = document.mathquiz.question7.value;
+	var question8 = document.mathquiz.question8.value;
+    var question9 = document.mathquiz.question9.value;
+    var question10 = document.mathquiz.question10.value;
     
     var correct = 0;
     
@@ -51,12 +51,12 @@ function check(){
 		correct++;
     }
 
-    var pictures = ["assets/images/result/trophy.gif", "assets/images/result/nice.gif"];
-	var messages = ["Conratulations!", "You are doing great!"];
+    var pictures = ["assets/images/result/trophy.gif", "assets/images/result/nice.gif","assets/images/result/retry.gif"];
+	var messages = ["Conratulations!", "You are doing great!,Don't give up!"];
 	var score;
 
 	if (correct == 0) {
-		score = 1;
+		score = 2;
 	}
 
 	if (correct > 0 && correct < 9) {
